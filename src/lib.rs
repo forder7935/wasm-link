c477e03eb6c39ef3fb069044ac8d88ab2d6bbbf2
Forder7935 <omni_github@proton.me>
2026-02-01 15:01:20 +0100

    copied README code snippet into src/lib.rs doc test; updated said snipped

--- src/lib.rs
+++ src/lib.rs
@@ -22,17 +22,92 @@
 //!   Other interfaces are internal - only accessible to plugins, not the host.
 //!
 //! # Usage
+//! ```
+//! use wasm_link::{
+//!     InterfaceData, InterfaceCardinality, FunctionData, ReturnKind,
+//!     PluginData, PluginTree, Socket, Engine, Component, Linker, Val,
+//! };
 //!
-//! 1. Implement [`InterfaceData`] and [`PluginData`] for your metadata source
-//!    (filesystem, database, embedded, etc.)
+//! // Declare your fixture sources
+//! #[derive( Clone )]
+//! struct Func { name: String, return_kind: ReturnKind }
+//! impl FunctionData for Func {
+//!     fn name( &self ) -> &str { self.name.as_str() }
+//!     fn return_kind( &self ) -> ReturnKind { self.return_kind.clone() }
+//!     // Determine whether a function is a resource method
+//!     // a constructor is not considered to be a method
+//!     fn is_method( &self ) -> bool { false }
+//! }
 //!
-//! 2. Build a [`PluginTree`] from your interfaces and plugins
+//! struct Interface { id: &'static str, funcs: Vec<Func> }
+//! impl InterfaceData for Interface {
+//!     type Id = &'static str ;
+//!     type Error = std::convert::Infallible ;
+//!     type Function = Func ;
+//!     type FunctionIter<'a> = std::slice::Iter<'a, Func> ;
+//!     type ResourceIter<'a> = std::iter::Empty<&'a String> ;
+//!     fn id( &self ) -> Result<&Self::Id, Self::Error> { Ok( &self.id ) }
+//!     fn cardinality( &self ) -> Result<&InterfaceCardinality, Self::Error> { Ok( &InterfaceCardinality::ExactlyOne ) }
+//!     fn package_name( &self ) -> Result<&str, Self::Error> { Ok( "my:package/example" ) }
+//!     fn functions( &self ) -> Result<Self::FunctionIter<'_>, Self::Error> { Ok( self.funcs.iter()) }
+//!     fn resources( &self ) -> Result<Self::ResourceIter<'_>, Self::Error> { Ok( std::iter::empty()) }
+//! }
 //!
-//! 3. Call [`PluginTree::load`] to compile WASM and link dependencies
+//! struct Plugin { id: &'static str, plug: &'static str }
+//! impl PluginData for Plugin {
+//!     type Id = &'static str ;
+//!     type InterfaceId = &'static str ;
+//!     type Error = std::convert::Infallible ;
+//!     type SocketIter<'a> = std::iter::Empty<&'a Self::InterfaceId> ;
+//!     fn id( &self ) -> Result<&Self::Id, Self::Error> { Ok( &self.id ) }
+//!     fn plug( &self ) -> Result<&Self::InterfaceId, Self::Error> { Ok( &self.plug ) }
+//!     fn sockets( &self ) -> Result<Self::SocketIter<'_>, Self::Error> { Ok( std::iter::empty()) }
+//!     fn component( &self, engine: &Engine ) -> Result<Component, Self::Error> {
+//!         /* inialise your component here */
+//! #       Ok( Component::new( engine, r#"(component
+//! #           (core module $m (func (export "f") (result i32) i32.const 42))
+//! #           (core instance $i (instantiate $m))
+//! #           (func $f (export "get-value") (result u32) (canon lift (core func $i "f")))
+//! #           (instance $inst (export "get-value" (func $f)))
+//! #           (export "my:package/example" (instance $inst))
+//! #       )"# ).unwrap())
+//!     }
+//! }
 //!
-//! 4. Use [`PluginTreeHead::dispatch`] to invoke functions on the root interface
+//! // Now construct some plugins and related data
+//! let root_interface_id = "root" ;
+//! let plugins = [ Plugin { id: "foo", plug: root_interface_id }];
+//! let interfaces = [ Interface { id: root_interface_id, funcs: vec![
+//!     Func { name: "get-value".to_string(), return_kind: ReturnKind::MayContainResources }
+//! ]}];
 //!
-//! See [`PluginTree`] and [`PluginTreeHead`] for usage examples.
+//! // First you need to tell wasm_link about your plugins, interfaces and where you want
+//! // the execution to begin. wasm_link will try it's best to load in all the plugins,
+//! // upon encountering an error, it will try to salvage as much of the remaining data
+//! // as possible returning a list of failures alongside the `PluginTree`.
+//! let ( tree, build_errors ) = PluginTree::new( root_interface_id, interfaces, plugins );
+//! assert!( build_errors.is_empty() );
+//!
+//! // Once you've got your `PluginTree` constructed, you can link the plugins together
+//! // Since some plugins may fail to load, it is only at this point that the cardinality
+//! // requirements are satisfied by the plugins that managed to get loaded, otherwise it
+//! // tries to salvage as much of the tree as can be loaded returning a list of failures
+//! // alongside the loaded `PluginTreeHead` - the root node of the `PluginTree`.
+//! let engine = Engine::default();
+//! let linker = Linker::new( &engine );
+//! let ( tree_head, load_errors ) = tree.load( &engine, &linker ).unwrap();
+//! assert!( load_errors.is_empty() );
+//!
+//! // Now you can dispatch any function on the root interface.
+//! // This will dispatch the function for all plugins plugged in to the root socket returning
+//! // a Result for each in the shape determined by the interface cardinality.
+//! let result = tree_head.dispatch( "my:package/example", "get-value", true, &[] );
+//! match result {
+//!     Socket::ExactlyOne( Ok( Val::U32( n ))) => assert_eq!( n, 42 ),
+//!     Socket::ExactlyOne( Err( e )) => panic!( "dispatch error: {e}" ),
+//!     _ => panic!( "unexpected cardinality" ),
+//! }
+//! ```
 
 mod interface ;
 mod plugin ;
